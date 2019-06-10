8f4634830cca7f8eef8843406639fbe5124b7727
Henry Harris IV <henryharrisiv@Henrys-MacBook-Pro-2.local>
2019-06-10 10:06:26 -0400

    updated jest configuration to import css files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-      </header>
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello world!!
     </div>
-  );
+    );
+  }
 }
-
-export default App;
